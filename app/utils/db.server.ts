c512cdb4ea0277ee8b75ae857d85017a621ef9af
Libor Gabrhel <gabrhel.libor@gmail.com>
2025-08-22 16:14:38 +0200

    fix: Remove console log for DATABASE_URL in db.server.ts

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -2,8 +2,6 @@ import { remember } from '@epic-web/remember'
 import { PrismaClient } from '@generated/prisma/client'
 import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3'
 
-console.log(process.env.DATABASE_URL)
-
 export const db = remember('db', () => {
   const adapter = new PrismaBetterSQLite3({
     url: `file:${process.env.DATABASE_PATH}`,
