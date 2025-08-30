3eb091fbf37c51bf301584b416ca2aad21781dc1
Libor Gabrhel <gabrhel.libor@gmail.com>
2025-08-30 21:35:39 +0200

    feat: Configure Vitest for testing with coverage and add pre-push checks

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: /^~\/(.+)/, replacement: './app/$1' },
+      { find: /^@generated\/(.+)/, replacement: './generated/$1' },
+    ],
+  },
+  test: {
+    coverage: {
+      all: true,
+      include: ['app/**/*.{ts,tsx}'],
+    },
+    include: ['./app/**/*.test.{ts,tsx}'],
+    restoreMocks: true,
+  },
+})
