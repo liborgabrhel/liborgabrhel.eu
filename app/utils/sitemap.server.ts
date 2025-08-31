43e06ec90849051a610f23c65a68f2571439f603
Libor Gabrhel <gabrhel.libor@gmail.com>
2025-08-31 08:36:32 +0200

    refactor: Rename variables for clarity and consistency in sitemap logic

--- app/utils/sitemap.server.ts
+++ app/utils/sitemap.server.ts
@@ -12,9 +12,9 @@ export async function getSitemapCount() {
 
 async function getTotalUrlsCount() {
   const totalStaticPages = STATIC_URLS.length
-  const totalBlogPosts = await db.note.count()
+  const totalNotes = await db.note.count()
 
-  return totalStaticPages + totalBlogPosts
+  return totalStaticPages + totalNotes
 }
 
 export async function getUrlsForSitemap(index: number) {
@@ -25,12 +25,13 @@ export async function getUrlsForSitemap(index: number) {
   return allUrls.slice(offset, offset + URLS_PER_SITEMAP)
 }
 
-const getBlogPostPathByType: Record<NoteType, (value: string) => string> = {
+const getNotesPathByType: Record<NoteType, (value: string) => string> = {
   BEEKEEPER: (slug) => href('/beekeeper/notes/:slug', { slug }),
   DEVELOPER: (slug) => href('/developer/notes/:slug', { slug }),
 }
 
 async function fetchAllUrls() {
+  // noinspection SpellCheckingInspection
   const urls: SitemapUrl[] = [
     ...STATIC_URLS.map((url) => ({
       changefreq: url.changefreq,
@@ -40,37 +41,37 @@ async function fetchAllUrls() {
     })),
   ]
 
-  const firstDeveloperBlogPost = await db.note.findFirst({
+  const latestDeveloperNote = await db.note.findFirst({
     orderBy: { updatedAt: 'desc' },
     select: { updatedAt: true },
     where: { type: 'DEVELOPER' },
   })
 
+  // noinspection SpellCheckingInspection
   urls.push({
     changefreq: 'monthly',
     lastmod:
-      firstDeveloperBlogPost?.updatedAt.toISOString() ||
-      new Date().toISOString(),
+      latestDeveloperNote?.updatedAt.toISOString() || new Date().toISOString(),
     path: href('/developer/notes'),
     priority: '0.8',
   })
 
-  const firstBeekeeperBlogPost = await db.note.findFirst({
+  const latestBeekeeperNote = await db.note.findFirst({
     orderBy: { updatedAt: 'desc' },
     select: { updatedAt: true },
     where: { type: 'BEEKEEPER' },
   })
 
+  // noinspection SpellCheckingInspection
   urls.push({
     changefreq: 'monthly',
     lastmod:
-      firstBeekeeperBlogPost?.updatedAt.toISOString() ||
-      new Date().toISOString(),
+      latestBeekeeperNote?.updatedAt.toISOString() || new Date().toISOString(),
     path: href('/beekeeper/notes'),
     priority: '0.8',
   })
 
-  const blogPosts = await db.note.findMany({
+  const notes = await db.note.findMany({
     select: {
       slug: true,
       type: true,
@@ -78,11 +79,12 @@ async function fetchAllUrls() {
     },
   })
 
+  // noinspection SpellCheckingInspection
   urls.push(
-    ...blogPosts.map((post) => ({
+    ...notes.map((note) => ({
       changefreq: 'monthly' as const,
-      lastmod: post.updatedAt.toISOString(),
-      path: getBlogPostPathByType[post.type](post.slug),
+      lastmod: note.updatedAt.toISOString(),
+      path: getNotesPathByType[note.type](note.slug),
       priority: '0.7',
     })),
   )
