e8ceb23f452b49d119159c48c8183435b8d2b3f2
Libor Gabrhel <gabrhel.libor@gmail.com>
2025-09-01 08:55:08 +0200

    refactor: Enhance RouteComponent to utilize loader data and export handlers

--- app/routes/beekeeper/notes/note/route.tsx
+++ app/routes/beekeeper/notes/note/route.tsx
@@ -1,3 +1,13 @@
-export default function RouteComponent() {
-  return <>Route</>
+// noinspection JSUnusedGlobalSymbols
+
+import type { Route } from './+types/route'
+
+export { handle } from './_handle'
+export { loader } from './_loader'
+export { meta } from './_meta'
+
+export default function RouteComponent({ loaderData }: Route.ComponentProps) {
+  const { note } = loaderData
+
+  return <>{note.title}</>
 }
--- app/routes/developer/notes/note/route.tsx
+++ app/routes/developer/notes/note/route.tsx
@@ -1,3 +1,5 @@
+export { handle } from './_handle'
+
 export default function RouteComponent() {
   return <>Route</>
 }
